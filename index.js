fd4a7d6529474b85d1b9a3dfcaf82693118d1596
MdFawjulAzim <mdfawjulazim617@gmail.com>
2025-09-16 16:43:44 +0600

    Controller and route file create

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-// index.js
-const { PrismaClient } = require("@prisma/client");
 const express = require("express");
-
-const prisma = new PrismaClient();
 const app = express();
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 app.use(express.json());
+
+// Routes
+const userRoutes = require("./routes/users");
+const bookRoutes = require("./routes/books");
+const reviewRoutes = require("./routes/reviews");
+const followRoutes = require("./routes/follows");
+
+app.use("/users", userRoutes);
+app.use("/books", bookRoutes);
+app.use("/reviews", reviewRoutes);
+app.use("/follows", followRoutes);
 
 app.listen(3000, () => console.log("Server running on http://localhost:3000"));
